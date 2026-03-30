4efe4a86523567f2ae1a473896212f45b578efe3
mehedyPUST <mehedy.pust@gmail.com>
2026-03-31 01:30:41 +0600

    test card period jhsdfjh

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,60 @@
+// import { Suspense, useState } from 'react'
+// import './App.css'
+// import Banner from './components/banner/Banner'
+// import Navbar from './components/navbar/Navbar'
+// import Products from './components/products/Products'
+// import Statistics from './components/statistics/Statistics'
+// import Cart from './components/cart/Cart'
+// import ProductsHeading from './components/products/productHeading/ProductsHeading'
+
+
+// const productsData = async () => {
+//   const res = await fetch('/productsData.json');
+//   return res.json();
+// }
+// const productsPromise = productsData();
+// function App() {
+//   const [activeTab, setActiveTab] = useState('products');
+//   const [cart, setCart] = useState([]);
+
+//   return (
+//     <>
+
+
+//       <div className=' bg-white  py-3 shadow-md  sticky top-0 z-50'>
+//         <Navbar></Navbar>
+//       </div>
+//       <Banner></Banner>
+//       <div className='mt-10'>
+//         <Statistics></Statistics>
+//       </div>
+//       <ProductsHeading setActiveTab={setActiveTab} activeTab={activeTab} setCart={setCart} cart={cart} ></ProductsHeading>
+//       <div className='mt-10'>
+//         <Suspense fallback={<div>Loading...........</div>}>
+
+//           {
+//             activeTab === 'products' ?
+//               <Products
+//                 productsPromise={productsPromise}
+//                 activeTab={activeTab}
+//                 setActiveTab={setActiveTab}
+//                 cart={cart}
+//                 setCart={setCart}
+//               ></Products>
+//               : <Cart cart={cart} setCart={setCart}></Cart>
+//           }
+//         </Suspense>
+//       </div>
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
 import { Suspense, useState } from 'react'
 import './App.css'
 import Banner from './components/banner/Banner'
@@ -21,27 +78,36 @@ function App() {
     <>
 
 
-      <div className=' bg-white  py-3 shadow-md  sticky top-0 z-50'>
-        <Navbar></Navbar>
-      </div>
-      <Banner></Banner>
-      <div className='mt-10'>
-        <Statistics></Statistics>
-      </div>
-      <ProductsHeading setActiveTab={setActiveTab} activeTab={activeTab} setCart={setCart} cart={cart} ></ProductsHeading>
+
       <div className='mt-10'>
         <Suspense fallback={<div>Loading...........</div>}>
 
           {
             activeTab === 'products' ?
-              <Products
-                productsPromise={productsPromise}
-                activeTab={activeTab}
-                setActiveTab={setActiveTab}
-                cart={cart}
-                setCart={setCart}
-              ></Products>
-              : <Cart cart={cart} setCart={setCart}></Cart>
+
+              <div>
+                <div className=' bg-white  py-3 shadow-md  sticky top-0 z-50'>
+                  <Navbar></Navbar>
+                </div>
+                <Banner></Banner>
+                <div className='mt-10'>
+                  <Statistics></Statistics>
+                </div>
+                <ProductsHeading setActiveTab={setActiveTab} activeTab={activeTab} setCart={setCart} cart={cart} ></ProductsHeading>
+
+                <Products
+                  productsPromise={productsPromise}
+                  activeTab={activeTab}
+                  setActiveTab={setActiveTab}
+                  cart={cart}
+                  setCart={setCart}
+                ></Products>
+              </div>
+              :
+              <div>
+                <ProductsHeading setActiveTab={setActiveTab} activeTab={activeTab} setCart={setCart} cart={cart} ></ProductsHeading>
+                <Cart cart={cart} setCart={setCart}></Cart>
+              </div>
           }
         </Suspense>
       </div>
@@ -51,5 +117,7 @@ function App() {
 }
 
 export default App
+
+
 
 
