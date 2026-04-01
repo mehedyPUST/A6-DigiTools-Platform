3eb6d86257d1b9f471205713e2f06371f05eb906
mehedyPUST <mehedy.pust@gmail.com>
2026-04-01 23:34:23 +0600

    donee

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -16,28 +16,32 @@ const Cart = ({ cart, setCart }) => {
     return (
         <div className="w-11/12 mx-auto">
             <h2 className="text-center text-xl font-bold mb-2">Your Cart</h2>
+
             {cart.length === 0 ? (
                 <EmptyCart></EmptyCart>
             ) : (
-                <div className="space-y-4   p-5 ">
+                <div className="space-y-4  ">
+                    <hr className='border-purple-600 mb-4 ' />
                     {cart.map(item => (
-                        <div key={item.id} className="shadow-xl border border-purple-600  bg-white rounded-md py-4 px-5 flex items-center justify-between">
+                        <div key={item.id} className="shadow-xl   bg-white rounded-md py-2 px-3 flex items-center justify-between">
                             <div className="flex gap-2 items-center">
-                                <img className="w-10 h-10 border border-purple-500 rounder-md" src={item.icon} alt={item.name} />
+                                <img className="w-10 h-10  shadow-sm rounded-md" src={item.icon} alt={item.name} />
                                 <div>
-                                    <p>{item.name}</p>
-                                    <p>Price: {item.price}</p>
+                                    <p className="text-sm md:text-base">{item.name}</p>
+                                    <p className="text-sm md:text-base">Price: {item.price}</p>
 
                                 </div>
                             </div>
                             <button onClick={() => removeFromCart(item.id, item.name)}
-                                className=" text-[#FF3980] cursor-pointer"> Remove</button>
+                                className=" text-[#FF3980] cursor-pointer text-sm md:text-base"> Remove</button>
                         </div>
                     ))}
+                    <hr className='border-purple-600 mt-4 ' />
                     <div className="flex justify-between items-center">
                         <h3 className="text-md font-bold ">Total: </h3>
                         <h3 className="text-md font-bold ">${total} </h3>
                     </div>
+
                     <div>
                         <button onClick={checkOutHandler} className="btn w-full  bg-linear-to-r from-purple-500 to-purple-700 text-white rounded-full "> Proceed To Checkout</button>
                     </div>
