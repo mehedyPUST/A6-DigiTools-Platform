b0513f8359aadc9015ada4560317b1370d529a9d
mehedyPUST <mehedy.pust@gmail.com>
2026-04-01 10:53:15 +0600

    pricing designd

--- src/components/products/productHeading/ProductsHeading.jsx
+++ src/components/products/productHeading/ProductsHeading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProductsHeading = ({ setActiveTab, activeTab, cart }) => {
     return (
         <div className=' w-11/12 mx-auto text-center space-y-2 mt-5'>
-            <h2 className='text-4xl font-bold'>Premium Digital Tools</h2>
+            <h2 className='text-2xl md:text-4xl font-bold'>Premium Digital Tools</h2>
             <p>Choose from our curated collection of premium digital products designed <br /> to boost your productivity and creativity.</p>
 
             <div className='flex justify-center mb-4 mt-4'>
